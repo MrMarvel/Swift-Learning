fc831937ef38f0772002a4130ebe77512b95a3be
MrMarvel <seregakkk999@yandex.ru>
2022-04-02 07:50:12 +0300

    Deprecation of storyboard

---
+++ NoStoryBoardApp/NoStoryBoardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoStoryBoardApp
+//
+//  Created by Mike I on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
